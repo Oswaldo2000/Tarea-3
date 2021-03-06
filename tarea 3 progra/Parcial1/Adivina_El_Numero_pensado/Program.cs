c3ad91685978181377906fd9c34f7b5a80bf4ef0
Oswaldo2000 <79265238+Oswaldo2000@users.noreply.github.com>
2021-03-05 18:10:28 -0600

    parcial1

---
+++ tarea 3 progra/Parcial1/Adivina_El_Numero_pensado/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Adivina_El_Numero_pensado
+{
+    class Program
+    {
+        static void Main(string[] args)
+            {
+            int numero, multipli, multipli2, suma, division, resta;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Ingrese un numero cualquiera");
+                numero = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+            Console.WriteLine("Multiplique el Numero por 2 ");
+            Console.ReadKey();
+
+            multipli = numero * 2;
+            Console.WriteLine("");
+            Console.WriteLine("Ahora sumele 8 a su resultado");
+            Console.ReadKey();
+            suma = multipli + 8;
+            Console.WriteLine("");
+            Console.WriteLine("Al resultado multipliquelo 5");
+            Console.ReadKey();
+            multipli2 = suma * 5;
+            Console.WriteLine(" ");
+            Console.WriteLine("El resultado final es: {0}", multipli2);
+            Console.WriteLine("Ahora procedere a adivinar tu numero");
+            Console.ReadKey();
+
+            division = multipli2 / 10;
+
+            resta = division - 4;
+            Console.WriteLine(" ");
+            Console.WriteLine("El numero que usted penso es: {0}", resta);
+            Console.ReadKey();
+    
+            
+            }
+        }
+    }
+
---
+++ tarea 3 progra/Parcial1/fecha decumpleaños.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace fecha_de_cumpleaños
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, multi1, multi2, suma1, suma2, resta, mes;
+            int q, w, e, z;
+
+            try
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("Escriba la fecha de nacimiento: ");
+                numero = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("     ");
+                Console.WriteLine("Ahora multipliquelo por 20");
+                Console.WriteLine("Presione enter para continuar");
+                Console.ReadKey();
+
+                multi1 = numero * 20;
+
+                Console.WriteLine("     ");
+                Console.WriteLine("Al resultado sumale 73");
+                Console.WriteLine("Presione enter para continuar");
+                Console.ReadKey();
+
+                suma1 = multi1 + 73;
+
+                Console.WriteLine("     ");
+                Console.WriteLine("Ahora multiplicalo por 5");
+                Console.WriteLine("Presione enter para continuar");
+                Console.ReadKey();
+
+                multi2 = suma1 * 5;
+
+                Console.WriteLine("     ");
+                Console.Write("Ingrese el mes de tu cumpleaños: ");
+                mes = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Sumale la fecha del mes de tu cumpleaños");
+
+                suma2 = multi2 + mes;
+
+                resta = suma2 - 365;
+
+                Console.WriteLine("     ");
+                Console.WriteLine("La fecha de tu cumpleaños es");
+                Console.WriteLine("Presione enter para continuar");
+                Console.ReadKey();
+
+                if (resta >= 1)
+                {
+                    if (resta <= 9999)
+                    {
+                        q = resta / 1000;
+                        e = (resta % 1000) / 100;
+                        w = (resta % 1000) % 100 / 10;
+                        z = ((resta % 1000) % 100) % 100 / 1;
+                        Console.WriteLine("La fecha de tu cumpleaños es: {0}{1}/{2}{3} ", q, e, w, z);
+                        Console.ReadKey();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Datos ingresados no validos");
+                Console.ReadKey();
+            }
+        }
+    }
+}
